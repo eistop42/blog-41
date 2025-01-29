7b33e2de2d6fa3e8534d61462998a17e272081c5
eistop42 <eistopacademy@gmail.com>
2025-01-29 20:55:33 +0500

    static, categories

--- core/views.py
+++ core/views.py
@@ -4,29 +4,32 @@ from django.http import HttpResponse
 
 from .models import Post, Feedback, PostCategory
 
-def main(request):
 
+def main(request):
     posts = Post.objects.all()
 
     category = request.GET.get('category')
+    active_category = None
 
     if category:
         posts = posts.filter(category__id=category)
+        active_category = PostCategory.objects.get(id=category)
 
     categories = PostCategory.objects.all()
 
-    return render(request, 'main.html', {'posts': posts, 'categories': categories})
+    return render(request, 'main.html', {'posts': posts,
+                                         'categories': categories,
+                                         'active_category': active_category
+                                         })
 
 
 def post_detail(request, post_id):
-
     post = Post.objects.get(id=post_id)
 
     return render(request, 'post_detail.html', {'post': post})
 
 
 def post_add(request):
-
     if request.method == 'POST':
         title = request.POST.get('title')
         text = request.POST.get('text')
@@ -35,11 +38,12 @@ def post_add(request):
 
         return redirect('main')
 
-    return render(request, 'post_add.html',)
+    categories = PostCategory.objects.all()
+
+    return render(request, 'post_add.html', {'categories': categories})
 
 
 def feedback(request):
-
     if request.method == 'POST':
         print(request.POST)
 
@@ -49,8 +53,8 @@ def feedback(request):
 
         return redirect('feedback_success')
 
-    return render(request, 'feedback.html',)
+    return render(request, 'feedback.html', )
+
 
 def feedback_success(request):
-
     return render(request, 'feedback_success.html', )
